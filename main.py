f17507773cf34d9072a36471417868fb2959bd9c
andy71 <andy71@users.noreply.github.com>
2024-03-04 15:50:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import urllib.request
 import json
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import os
 from datetime import datetime
 import logging
@@ -10,6 +7,7 @@ import logging
 # Logger konfigurieren
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
+
 # Erhalte den absoluten Pfad zur aktuellen Datei
 current_directory = os.path.dirname(os.path.abspath(__file__))
 CONFIG_FILE = os.path.join(current_directory, 'config.json')
@@ -21,16 +19,7 @@ def load_config():
         print("Ein Beispiel für die Konfigurationsdatei könnte wie folgt aussehen:")
         print("{")
         print("    \"api_key\": \"YOUR-API-KEY\",")
-        print("    \"sender_email\": \"sender@example.de\",")
-        print("    \"email_password\": \"YOUR-EMAIL-PASSWORD\",")
-        print("    \"smtp_server\": \"smtp.gmail.com\",")
-        print("    \"smtp_port\": 465,")
-        print("    \"receiver_emails\": [")
-        print("        \"reciver1@example.de\",")
-        print("        \"reciver2@example.de\"")
-        print("    ],")
         print("    \"message_users_fremdschluessel\": \"1000,1001\",")
-        print("    \"message_rics\": \"22,23\",")
         print("    \"status_dict\": {")
         print("        ")
         print("    }")
@@ -40,82 +29,45 @@ def load_config():
         config = json.load(f)
     return config
 
-
 def save_config(config):
     with open(CONFIG_FILE, 'w') as f:
         json.dump(config, f, indent=4)
 
-def send_email(content,shortname, sender_email, receiver_emails, password, smtp_server, smtp_port):
-    # E-Mail Inhalt erstellen
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = ", ".join(receiver_emails)
-    msg['Subject'] = "Änderung Fahrzeugstatus " + shortname + "!"
-
-    body = content
-    msg.attach(MIMEText(body, 'plain'))
-
-    # E-Mail Server einrichten und verbinden
-    try:
-        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
-        server.ehlo()
-        server.login(sender_email, password)
-
-        # E-Mail senden
-        text = msg.as_string()
-        server.sendmail(sender_email, receiver_emails, text)
-        logger.info("E-Mail erfolgreich gesendet!")
-    except smtplib.SMTPAuthenticationError as auth_error:
-        logger.error("Fehler: Authentifizierung fehlgeschlagen. Stelle sicher, dass Benutzername und Passwort korrekt sind.")
-        logger.error("Details: %s", auth_error)
-    except smtplib.SMTPException as smtp_error:
-        logger.error("Fehler beim Senden der E-Mail.")
-        logger.error("Details: %s", smtp_error)
-    except Exception as e:
-        logger.error("Ein allgemeiner Fehler ist aufgetreten:", e)
-    finally:
-        if 'server' in locals():
-            server.quit()
-
-def send_push(shortname, message_text, api_key, message_users_fremdschluessel, message_rics):
+def send_push(shortname, message_text, api_key, message_users_fremdschluessel):
     message_titel = "Änderung Fahrzeugstatus " + shortname + "!"
     if message_users_fremdschluessel != "":
         message_url = f"https://app.divera247.com/api/news?title={urllib.parse.quote(message_titel)}&text={urllib.parse.quote(message_text)}&person={message_users_fremdschluessel}&accesskey={api_key}"
-        urllib.request.urlopen(message_url)
-        logger.info("Mitteilung erfolgreich versendet.")
+        try:
+            urllib.request.urlopen(message_url)
+            logger.info("Mitteilung erfolgreich versendet.")
+        except Exception as e:
+            logger.error("Fehler beim Senden der Mitteilung:", e)
+
     else:
         logger.info("Keine Divera User angegeben. Mitteilung wird nicht versendet.")
-    if message_rics != "":
-        message_url = f"https://app.divera247.com/api/news?title={urllib.parse.quote(message_titel)}&text={urllib.parse.quote(message_text)}&ric={message_rics}&accesskey={api_key}"
-        urllib.request.urlopen(message_url)
-        logger.info("Mitteilung erfolgreich versendet.")
-    else:
-        logger.info("Keine Divera Rics angegeben. Mitteilung wird nicht versendet.")
 
 def main():
     config = load_config()
     api_key = config["api_key"]
     message_users_fremdschluessel = config["message_users_fremdschluessel"]
-    message_rics = config["message_rics"]
-    sender_email = config["sender_email"]
-    receiver_emails = config["receiver_emails"]
-    password = config["email_password"]
-    smtp_server = config["smtp_server"]
-    smtp_port = config["smtp_port"]
-    url = f"https://app.divera247.com/api/v2/pull/vehicle-status?accesskey={api_key}"
+    url = f"https://app.divera247.com/api/v2/pull/all?accesskey={api_key}"
 
-    logger.info("Skript gestartet.")
+    logger.info("Script gestartet.")
+
     # Status jeder ID speichern
     status_dict = config.get("status_dict", {})
-
     try:
         with urllib.request.urlopen(url) as response:
-            data = json.loads(response.read().decode())
-            for item in data["data"]:
-                id = str(item["id"])  # ID als Zeichenkette speichern
-                fullname = item["fullname"]
+            json_data = json.loads(response.read().decode())
+            fms_states = json_data["data"]["cluster"]["fms_status"]
+            vehicles_data = json_data["data"]["cluster"]["vehicle"]
+            for item_id in vehicles_data:
+                item = vehicles_data[item_id]
+                id = str(item_id)  # ID als Zeichenkette speichern
+                name = item["name"]
                 shortname = item["shortname"]
-                fmsstatus = item["fmsstatus"]
+                fmsstatus = item["fmsstatus_id"]
+                vehicle_state_change_dt = datetime.fromtimestamp(item["fmsstatus_ts"]).strftime('%Y-%m-%d %H:%M:%S')
 
                 # Wenn die ID noch nicht im status_dict ist, füge sie hinzu
                 if id not in status_dict:
@@ -123,21 +75,13 @@ def main():
                     logger.info("ID wurde hinzugefügt. Aktueller status_dict:", status_dict)
                     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} | ID wurde hinzugefügt. Aktueller status_dict:", status_dict)
                 else:
-                    # Wenn sich der Status von 6 auf != 6 oder von !=6 auf 6 ändert, sende eine E-Mail und aktualisiere den Status
-                    if (status_dict[id] == 6 and fmsstatus != 6) or (status_dict[id] != 6 and fmsstatus == 6):
-                        if status_dict[id] == 6:
-                            message = f"Ein Fahrzeug ({shortname}) ist jetzt wieder einsatzbereit. \nID: {id},\n Fahrzeugname: {fullname},\n Kurzname: {shortname},\n FMS Status: {fmsstatus}\n"
-                        else:
-                            message = f"Ein Fahrzeug ({shortname}) ist aktuell nicht einsatzbereit. \nID: {id},\n Fahrzeugname: {fullname},\n Kurzname: {shortname},\n FMS Status: {fmsstatus}\n"
+                    # Wenn sich der Status ändert, ...
+                    if (status_dict[id] != fmsstatus):
+                        message = f"Status von Fahrzeug wurde geändert.\nFahrzeugname: {name}\nKurzname: {shortname}\n"
+                        message += f"Neuer Status (seit {vehicle_state_change_dt}): {fms_states['items'][str(fmsstatus)]['name']} ({fmsstatus})\n"
 
-                        # E-Mail senden
-                        if receiver_emails:
-                            # E-Mail senden
-                            send_email(message, shortname, sender_email, receiver_emails, password, smtp_server, smtp_port)
-                        else:
-                            logger.info("Keine Empfänger-E-Mail-Adressen angegeben. E-Mail wird nicht versendet.")
                         # Pushnachricht senden
-                        send_push(shortname, message, api_key,message_users_fremdschluessel,message_rics)
+                        send_push(shortname, message, api_key,message_users_fremdschluessel)
                     # Aktualisiere den Status für die ID
                     status_dict[id] = fmsstatus
 
