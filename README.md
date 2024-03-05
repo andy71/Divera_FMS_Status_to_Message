e873ed638bd44306d64f98a528d309aa450f0a07
Andreas Nadobnik <github@nadobnik.de>
2024-03-05 12:37:48 +0100

    - Umstellung auf Divera API v2 - Mitteilungen enthalten jetzt den PrivateMode und FMS Koordinaten - Autoarchivierung kann genuttz werden - kleinere Anpassungen

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# Divera FMS Status Änderungen via Divera 24/7 Mitteilung.
+# Divera FMS Status Änderungen via Divera 24/7 Mitteilung
 
 Dieses Script ist eine Vereinfachung des Repos [Sleepwalker86/Divera_FMS_Status_to_Email](https://github.com/Sleepwalker86/Divera_FMS_Status_to_Email).
 
-Dieser Fork sendet nur eine Divera Mitteilung an eine Liste von Benutzer Fremdschlüsseln wann immer sich der Fahrzeugstatus ändert.
+Dieser Fork sendet nur noch Divera Mitteilungen an eine Liste von Benutzern (via Fremdschlüssel) wann immer sich der Fahrzeugstatus ändert.
+
+Darüber hinaus kann die Autoarchivierung genutzt werden um die Mitteilungen nach konfigurierbarer Zeit in das Archiv zu verschieben.
 
 Kopiert die config-example.json zu config.json und passt die Beispiel Parameter an.
 
-Beispiel Config:
-
+## config-example.json
 ```json
 {
-    "_comment": "Diese Datei bitte kopieren und entsprechend anpassen bzw. ausfüllen. STATUS_DICT nicht ändern. Anschließend die datei in config.json umbenennen!",
+    "_comment": "Diese Datei bitte kopieren und entsprechend anpassen bzw. ausfüllen. STATUS_DICT nicht ändern. Anschließend die Datei in config.json umbenennen!",
     "api_key": "YOUR-API-KEY",
-    "message_users_fremdschluessel": "1000,1001",
+    "message_users_fremdschluessel": ["1000", "1001"],
+    "autoarchive_days": 0,
+    "autoarchive_hours": 0,
+    "autoarchive_minutes": 0,
+    "autoarchive_seconds": 0,
     "status_dict": {}
 }
 ```
+Die "autoarchive_*-Einträge sind optional.
+Sie können beliebig kombiniert und genutzt werden um die Mitteilung automatisch archivieren zu lassen.
 
-
-# Der Cronjob zum Starten könnte so aussehen (contab -e):
+# Cronjob
+Der Cronjob zum Starten könnte so aussehen (contab -e):
 
 ```crontab
 # Alle 5 Min prüfen ob sich ein Fahrzeug Status geändert hat
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 import os
 from datetime import datetime
+import time
 import logging
 
 # Logger konfigurieren
@@ -14,16 +15,20 @@ CONFIG_FILE = os.path.join(current_directory, 'config.json')
 
 def load_config():
     if not os.path.exists(CONFIG_FILE):
-        print(f"Die Konfigurationsdatei '{CONFIG_FILE}' existiert nicht.")
-        print("Bitte erstellen Sie eine Konfigurationsdatei mit den erforderlichen Informationen.")
-        print("Ein Beispiel für die Konfigurationsdatei könnte wie folgt aussehen:")
-        print("{")
-        print("    \"api_key\": \"YOUR-API-KEY\",")
-        print("    \"message_users_fremdschluessel\": \"1000,1001\",")
-        print("    \"status_dict\": {")
-        print("        ")
-        print("    }")
-        print("}")
+        logger.error(f'Die Konfigurationsdatei "{CONFIG_FILE}" existiert nicht.')
+        logger.info('Bitte erstellen Sie eine Konfigurationsdatei mit den erforderlichen Informationen.')
+        logger.info('Ein Beispiel für die Konfigurationsdatei könnte wie folgt aussehen:')
+        logger.info('{')
+        logger.info('    "api_key": "YOUR-API-KEY",')
+        logger.info('    "message_users_fremdschluessel": ["1000","1001"],')
+        logger.info('    "autoarchive_days": 0,')
+        logger.info('    "autoarchive_hours": 0,')
+        logger.info('    "autoarchive_minutes": 0,')
+        logger.info('    "autoarchive_seconds": 0,')
+        logger.info('    "status_dict": {}')
+        logger.info('}')
+        logger.info('Die "autoarchive_*-Einträge sind optional.')
+        logger.info('Sie können beliebig kombiniert und genutzt werden um die Mitteilung automatisch archivieren zu lassen.')
         exit(1)  # Beenden des Skripts mit Fehlercode 1
     with open(CONFIG_FILE) as f:
         config = json.load(f)
@@ -33,16 +38,63 @@ def save_config(config):
     with open(CONFIG_FILE, 'w') as f:
         json.dump(config, f, indent=4)
 
-def send_push(shortname, message_text, api_key, message_users_fremdschluessel):
-    message_titel = "Änderung Fahrzeugstatus " + shortname + "!"
+def send_push_v2(shortname, message_text, api_key, message_users_fremdschluessel, lat=0, lng=0, autoarchive_days=0, autoarchive_hours=0, autoarchive_minutes=0, autoarchive_seconds=0):
     if message_users_fremdschluessel != "":
-        message_url = f"https://app.divera247.com/api/news?title={urllib.parse.quote(message_titel)}&text={urllib.parse.quote(message_text)}&person={message_users_fremdschluessel}&accesskey={api_key}"
-        try:
-            urllib.request.urlopen(message_url)
-            logger.info("Mitteilung erfolgreich versendet.")
-        except Exception as e:
-            logger.error("Fehler beim Senden der Mitteilung:", e)
+        if (autoarchive_days or autoarchive_hours or autoarchive_minutes or autoarchive_seconds):
+            archive = True
+            ts_archive = int(time.time())
+            ts_archive += autoarchive_seconds
+            ts_archive += (autoarchive_minutes * 60)
+            ts_archive += (autoarchive_hours * 60 * 60)
+            ts_archive += (autoarchive_days * 60 * 60 * 24)
+        else:
+            archive = False
+            ts_archive = ''
 
+        body={
+            "News": {
+                "title": "Änderung Fahrzeugstatus " + shortname + "!",
+                "text": message_text,
+                "address": "",
+                "lat": lat,
+                "lng": lng,
+                "survey": False,
+                "private_mode": True,
+                "notification_type": 4,
+                "send_push": True,
+                "send_sms": False,
+                "send_call": False,
+                "send_mail": False,
+                "send_pager": False,
+                "archive": archive,
+                "ts_archive": ts_archive,
+                "cluster": [],
+                "group": [],
+                "user_cluster_relation": [
+                    message_users_fremdschluessel
+                ]
+            },
+            "instructions": {
+                "user_cluster_relation": {
+                    "mapping": "foreign_id"
+                }
+            }
+        }
+        logger.debug(json.dumps(body, indent=4))
+
+        message_url = f"https://app.divera247.com/api/v2/news?accesskey={api_key}"
+        try:
+            req = urllib.request.Request(message_url)
+            req.add_header('Content-Type', 'application/json; charset=utf-8')
+            jsondataasbytes = json.dumps(body).encode('utf-8')
+            req.add_header('Content-Length', len(jsondataasbytes))
+            print("foo")
+            response = urllib.request.urlopen(req, jsondataasbytes)
+            logger.info("Mitteilung erfolgreich versendet.")
+            logger.info(response.read())
+        except Exception as e:
+            logger.error("Fehler beim Senden der Mitteilung:")
+            logger.error(e)
     else:
         logger.info("Keine Divera User angegeben. Mitteilung wird nicht versendet.")
 
@@ -50,6 +102,23 @@ def main():
     config = load_config()
     api_key = config["api_key"]
     message_users_fremdschluessel = config["message_users_fremdschluessel"]
+    if 'autoarchive_days' in config:
+        autoarchive_days = config['autoarchive_days']
+    else:
+        autoarchive_days = 0
+    if 'autoarchive_hours' in config:
+        autoarchive_hours = config['autoarchive_hours']
+    else:
+        autoarchive_hours = 0
+    if 'autoarchive_minutes' in config:
+        autoarchive_minutes = config['autoarchive_minutes']
+    else:
+        autoarchive_minutes = 0
+    if 'autoarchive_seconds' in config:
+        autoarchive_seconds = config['autoarchive_seconds']
+    else:
+        autoarchive_seconds = 0
+
     url = f"https://app.divera247.com/api/v2/pull/all?accesskey={api_key}"
 
     logger.info("Script gestartet.")
@@ -67,30 +136,48 @@ def main():
                 name = item["name"]
                 shortname = item["shortname"]
                 fmsstatus = item["fmsstatus_id"]
+                lat= item["lat"]
+                lng= item["lng"]
                 vehicle_state_change_dt = datetime.fromtimestamp(item["fmsstatus_ts"]).strftime('%Y-%m-%d %H:%M:%S')
 
                 # Wenn die ID noch nicht im status_dict ist, füge sie hinzu
                 if id not in status_dict:
                     status_dict[id] = fmsstatus
-                    logger.info("ID wurde hinzugefügt. Aktueller status_dict:", status_dict)
-                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} | ID wurde hinzugefügt. Aktueller status_dict:", status_dict)
+                    logger.info(f"ID wurde hinzugefügt. Aktueller status_dict: {status_dict}")
                 else:
                     # Wenn sich der Status ändert, ...
                     if (status_dict[id] != fmsstatus):
                         message = f"Status von Fahrzeug wurde geändert.\nFahrzeugname: {name}\nKurzname: {shortname}\n"
                         message += f"Neuer Status (seit {vehicle_state_change_dt}): {fms_states['items'][str(fmsstatus)]['name']} ({fmsstatus})\n"
+                        if (lat and lng):
+                            message += f"\nPosition: {lat}, {lng}"
 
                         # Pushnachricht senden
-                        send_push(shortname, message, api_key,message_users_fremdschluessel)
+                        send_push_v2(
+                            shortname,
+                            message,
+                            api_key,
+                            message_users_fremdschluessel,
+                            lat=lat,
+                            lng=lng,
+                            autoarchive_days=autoarchive_days,
+                            autoarchive_hours=autoarchive_hours,
+                            autoarchive_minutes=autoarchive_minutes,
+                            autoarchive_seconds=autoarchive_seconds
+                        )
                     # Aktualisiere den Status für die ID
                     status_dict[id] = fmsstatus
 
         # Speichere den Status in der Konfigurationsdatei
         config["status_dict"] = status_dict
         save_config(config)
-
     except Exception as e:
-        logger.error("Fehler beim Abrufen der Daten oder beim Senden der E-Mail:", e)
+        logger.error("Fehler beim Abrufen der Daten:\n", e)
+
 if __name__ == "__main__":
-    main()
-    logger.info("Script erfolgreich ausgeführt!")
+    try:
+        main()
+        logger.info("Script erfolgreich ausgeführt.")
+    except Exception as e:
+        logger.error("Fehler beim Ausführen des Scripts:\n", e)
+        exit(1)
